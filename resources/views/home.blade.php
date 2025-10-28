f92170220a0fb9e671d032a525afb3b51c08deaa
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2025-10-28 12:24:29 +0100

    feat: add first components inside the project.
    + add new navbar with divided part like, logo menu link and link to create new job.
    
    + add new tag section in the home page.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<x-layout>
+    <secion>
+        <h3>Top Jobs</h3>
+        <hr>
+        <div class="border mt-10 max-w-[350px] rounded-xl p-4 bg-white/10">
+
+            <!-- TOP HEAD -->
+            <div class="text-sm mb-2">
+                <p>Employer Name</p>
+            </div>
+            <!-- CENTER HEAD -->
+            <div class="text-base font-bold mb-2">
+                <!-- JOB TITLE -->
+                <h3>Full Stack Laravel Developer</h3>
+                <p>Full time - From $60.000</p>
+            </div>
+
+            <!-- BOTTOM HEAD -->
+            <div class="mb-2">
+                <div>
+                   <a href="#"> Tag </a>
+                   <a href="#"> Tag </a>
+                   <a href="#"> Tag </a>
+                </div>
+                <img src="http://placehold.it/42/42" alt=""/>
+            </div>
+        </div>
+    </secion>
+</x-layout>

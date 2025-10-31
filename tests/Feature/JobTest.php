4654af1b316ad9d6c28899a4d66e90228834b40d
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2025-10-31 12:40:46 +0100

    feat(add-new-test): create new test function.
    + Check if the employer owns the job and if the job its own either for the use.

--- tests/Feature/JobTest.php
+++ tests/Feature/JobTest.php
@@ -2,6 +2,7 @@
 
 use App\Models\Employer;
 use App\Models\Job;
+use App\Models\User;
 
 it('belong to an employer', function () {
     // AAA
@@ -18,8 +19,43 @@ it('belong to an employer', function () {
 
 it('can have tags', function () {
     $job = Job::factory()->create();
-    
+
     $job->tag('Frontend');
 
     expect($job->tags)->toHaveCount(1);
+});
+
+it('belong to a employer', function () {
+    // Arrange the world
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create([
+        'employer_id' => $employer
+    ]);
+
+    // Act
+    $job_to_employer = $job->employer()->is($employer);
+
+    // Assert that the employer belong to the job
+    expect($job_to_employer)->toBeTrue();
+
+});
+
+it('belong to a user', function () {
+    // Arrange the world
+    $user = User::factory()->create();
+
+    $employer = Employer::factory()->create([
+        'user_id' => $user
+    ]);
+
+    $job = Job::factory()->create([
+        'employer_id' => $employer
+    ]);
+
+    // Act
+    $job_to_user = $job->employer()->is($employer);
+
+    // Assert that the employer belong to the job
+    expect($job_to_user)->toBeTrue();
+
 });
